37138a28b00d13770fec0155a2a49d135f64abaa
nexus <75250667+thscodeisvryg0od@users.noreply.github.com>
2026-09-23 19:38:36 +0300

    Create AppInfo class to hold application details

---
+++ app/src/main/java/com/example/launcher16/AppInfo.java
@@ -0,0 +1,9 @@
+package com.example.launcher16;
+
+import android.graphics.drawable.Drawable;
+
+public class AppInfo {
+    public String label;
+    public String packageName;
+    public Drawable icon;
+}
