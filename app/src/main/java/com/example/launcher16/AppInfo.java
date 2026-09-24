62e1c4ebff690f737c85e52cb3cafbfb149fcaf6
Fırat Yaman <75250667+thscodeisvryg0od@users.noreply.github.com>
2026-09-24 11:56:31 +0300

    Delete AppInfo.java

--- app/src/main/java/com/example/launcher16/AppInfo.java
+++
@@ -1,9 +0,0 @@
-package com.example.launcher16;
-
-import android.graphics.drawable.Drawable;
-
-public class AppInfo {
-    public String label;
-    public String packageName;
-    public Drawable icon;
-}
